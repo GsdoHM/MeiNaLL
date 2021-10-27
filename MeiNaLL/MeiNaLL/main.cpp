6b76cd2d165f724b729a8690c4cddb0f1969c0c8
erlosshex <gugsabel@163.com>
2021-10-28 00:10:20 +0800

    git add first modify

--- MeiNaLL/MeiNaLL/main.cpp
+++ MeiNaLL/MeiNaLL/main.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
+
 #include <QtCore/QCoreApplication>
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+	std::cout << "hello, world!" << std::endl;
 
     return a.exec();
 }
